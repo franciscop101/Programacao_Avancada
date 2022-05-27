adf270c916937a02618988498e27a2df5ccc6e4a
franciscop101 <83250815+franciscop101@users.noreply.github.com>
2022-05-27 11:55:46 +0100

    inicial layout

--- app/src/main/java/com/example/trabalho/MainActivity.kt
+++ app/src/main/java/com/example/trabalho/MainActivity.kt
@@ -13,14 +13,14 @@ import com.example.trabalho.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
 
-    private lateinit var appBarConfiguration: AppBarConfiguration
-    private lateinit var binding: ActivityMainBinding
+        private lateinit var appBarConfiguration: AppBarConfiguration
+        private lateinit var binding: ActivityMainBinding
 
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
+        override fun onCreate(savedInstanceState: Bundle?) {
+            super.onCreate(savedInstanceState)
 
-        binding = ActivityMainBinding.inflate(layoutInflater)
-        setContentView(binding.root)
+            binding = ActivityMainBinding.inflate(layoutInflater)
+            setContentView(binding.root)
 
         setSupportActionBar(binding.toolbar)
 
