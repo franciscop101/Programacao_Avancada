fe3091d2d1cf1eef2648fc4d9198d659d96d2113
franciscop101 <83250815+franciscop101@users.noreply.github.com>
2022-06-02 20:38:16 +0100

    clientes (com a função cria para criar a tabela)

---
+++ app/src/main/java/com/example/trabalho/TabelaBDClient.kt
@@ -0,0 +1,23 @@
+package com.example.trabalho
+
+
+import android.database.sqlite.SQLiteDatabase
+import android.provider.BaseColumns
+
+class TabelaBDClient (val db: SQLiteDatabase) {
+    fun cria() {
+        db.execSQL("CREATE TABLE $NOME (${BaseColumns._ID} INTEGER PRIMARY KEY AUTOINCREMENT," +
+                " $CAMPO_NOME TEXT NOT NULL,"+
+                " $CAMPO_DATE_BIRTH DATE NOT NULL,"+
+                " $CAMPO_PHONE_NUMBER NUMBER NOT NULL,"+
+                " $CAMPO_EMAIL TEXT NOT NULL)")
+    }
+
+    companion object {
+        const val NOME = "client"
+        const val CAMPO_NOME = "nome"
+        const val CAMPO_DATE_BIRTH = "date_birth"
+        const val CAMPO_PHONE_NUMBER ="phone_number"
+        const val CAMPO_EMAIL = "email"
+    }
+}
