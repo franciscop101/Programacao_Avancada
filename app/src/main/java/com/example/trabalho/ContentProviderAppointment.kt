f29252479bbb9ad086f59f967f9604e49f1a0118
franciscop101 <83250815+franciscop101@users.noreply.github.com>
2022-06-21 20:12:53 +0100

    Implementada a função insert do ContentProviderAppointment

--- app/src/main/java/com/example/trabalho/ContentProviderAppointment.kt
+++ app/src/main/java/com/example/trabalho/ContentProviderAppointment.kt
@@ -180,7 +180,21 @@ class ContentProviderAppointment : ContentProvider() {
      * @return The URI for the newly inserted item.
      */
     override fun insert(uri: Uri, values: ContentValues?): Uri? {
-        TODO("Not yet implemented")
+        val db = dbOpenHelper!!.writableDatabase
+
+        requireNotNull(values)
+
+        val id = when (getUriMatcher().match(uri)) {
+            URI_SERVICES -> TabelaBDService(db).insert(values)
+            URI_CLIENTS -> TabelaBDClient(db).insert(values)
+            else -> -1
+        }
+
+        db.close()
+
+        if (id == -1L) return null
+
+        return Uri.withAppendedPath(uri, "$id")
     }
 
     /**
