9d0829d27f6d3b93b2943b26e195338f38090c7f
franciscop101 <83250815+franciscop101@users.noreply.github.com>
2022-06-21 20:25:43 +0100

    Implementada a função update do ContentProviderAppointment

--- app/src/main/java/com/example/trabalho/ContentProviderAppointment.kt
+++ app/src/main/java/com/example/trabalho/ContentProviderAppointment.kt
@@ -257,7 +257,21 @@ class ContentProviderAppointment : ContentProvider() {
         selection: String?,
         selectionArgs: Array<out String>?
     ): Int {
-        TODO("Not yet implemented")
+        requireNotNull(values)
+
+        val db = dbOpenHelper!!.writableDatabase
+
+        val id = uri.lastPathSegment
+
+        val registosAlterados = when (getUriMatcher().match(uri)) {
+            URI_SERVICE_ESPECIFICO -> TabelaBDService(db).update(values, "${BaseColumns._ID}=?", arrayOf("${id}"))
+            URI_CLIENT_ESPECIFICA -> TabelaBDClient(db).update(values,"${BaseColumns._ID}=?", arrayOf("${id}"))
+            else -> 0
+        }
+
+        db.close()
+
+        return registosAlterados
     }
 
     companion object {
