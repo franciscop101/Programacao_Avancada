f879fecd9f1986ed9223f7cd65761328be11b63b
franciscop101 <83250815+franciscop101@users.noreply.github.com>
2022-06-28 16:29:36 +0100

    Correçao de erros (Android X)

--- app/src/main/java/com/example/trabalho/MainActivity.kt
+++ app/src/main/java/com/example/trabalho/MainActivity.kt
@@ -2,7 +2,7 @@ package com.example.trabalho
 
 import android.os.Bundle
 /**import android.support.design.widget.Snackbar*/
-import android.support.v7.app.AppCompatActivity
+import androidx.appcompat.app.AppCompatActivity
 import androidx.navigation.findNavController
 import androidx.navigation.ui.AppBarConfiguration
 import androidx.navigation.ui.navigateUp
--- app/src/main/java/com/example/trabalho/SecondFragment.kt
+++ app/src/main/java/com/example/trabalho/SecondFragment.kt
@@ -1,7 +1,7 @@
 package com.example.trabalho
 
 import android.os.Bundle
-import android.support.v4.app.Fragment
+import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
@@ -32,9 +32,7 @@ class SecondFragment : Fragment() {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
-        binding.buttonSecond.setOnClickListener {
-            findNavController().navigate(R.id.action_SecondFragment_to_FirstFragment)
-        }
+
     }
 
     override fun onDestroyView() {
