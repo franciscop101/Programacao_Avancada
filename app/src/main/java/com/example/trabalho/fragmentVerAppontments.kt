38de057e0bbb7be50d9f71e82a4aaf3dfe189b9b
franciscop101 <83250815+franciscop101@users.noreply.github.com>
2022-07-04 22:33:37 +0100

    criaçao de fragmentoSeeApointment

--- app/src/main/java/com/example/trabalho/fragmentVerAppontments.kt
+++ app/src/main/java/com/example/trabalho/fragmentVerAppontments.kt
@@ -1,23 +1,18 @@
 package com.example.trabalho
 
-import android.database.Cursor
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import androidx.loader.app.LoaderManager
-import androidx.loader.content.CursorLoader
-import androidx.loader.content.Loader
 import androidx.navigation.fragment.findNavController
-import com.example.trabalho.databinding.FragmentSecondBinding
+import com.example.trabalho.databinding.FragmentVerAppointmentBinding
 
 /**
- * A simple [Fragment] subclass as the second destination in the navigation.
+ * A simple [Fragment] subclass as the default destination in the navigation.
  */
-class SecondFragment : Fragment(){
-
-    private var _binding: FragmentSecondBinding? = null
+class fragmentVerAppontments : Fragment() {
+    private var _binding: FragmentVerAppointmentBinding? = null
 
     // This property is only valid between onCreateView and
     // onDestroyView.
@@ -28,7 +23,7 @@ class SecondFragment : Fragment(){
         savedInstanceState: Bundle?
     ): View? {
 
-        _binding = FragmentSecondBinding.inflate(inflater, container, false)
+        _binding = FragmentVerAppointmentBinding.inflate(inflater, container, false)
         return binding.root
 
     }
@@ -36,17 +31,14 @@ class SecondFragment : Fragment(){
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
-        //LoaderManager.getInstance(this).initLoader(ID_LOADER_APPOINTMENTS, null, this)
-
-        binding.buttonEnviar2.setOnClickListener {
+        binding.buttonEnviar3.setOnClickListener {
             findNavController().navigate(R.id.action_SecondFragment_to_FirstFragment)
         }
+
     }
 
     override fun onDestroyView() {
         super.onDestroyView()
         _binding = null
     }
-
-
 }
